c8a5790442974c011df9f5ebb3893c773cbd5319
Chavez <matthew@el-chavez.me>
2017-06-30 18:34:51 -0700

    Add basic filter configuration
    - Add config options type
    - Implement New function to create a new filter
    - Set defaults for any option not configured

---
+++ config.go
@@ -0,0 +1,71 @@
+package cuckoo
+
+import (
+	"hash"
+	"hash/fnv"
+)
+
+type configOption func(*Filter)
+
+const (
+	defaultBucketEntries     uint = 8
+	defaultBucketTotal       uint = 2 ^ 30
+	defaultFingerprintLength uint = 6
+	defaultKicks             uint = 500
+)
+
+var (
+	defaultHasher = fnv.New64()
+)
+
+func BucketEntries(entries uint) configOption {
+	return func(f *Filter) {
+		f.bucketEntries = entries
+	}
+}
+
+func BucketTotal(total uint) configOption {
+	return func(f *Filter) {
+		f.bucketTotal = total
+	}
+}
+
+func FingerprintLength(length uint) configOption {
+	return func(f *Filter) {
+		f.fingerprintLength = length
+	}
+}
+
+func Hasher(hasher hash.Hash) configOption {
+	return func(f *Filter) {
+		f.hasher = hasher
+	}
+}
+
+func Kicks(kicks uint) configOption {
+	return func(f *Filter) {
+		f.kicks = kicks
+	}
+}
+
+func (f *Filter) configureDefaults() {
+	if f.bucketEntries <= 0 {
+		BucketEntries(defaultBucketEntries)(f)
+	}
+
+	if f.bucketTotal <= 0 {
+		BucketEntries(defaultBucketTotal)(f)
+	}
+
+	if f.fingerprintLength <= 0 {
+		FingerprintLength(defaultFingerprintLength)(f)
+	}
+
+	if f.hasher == nil {
+		Hasher(defaultHasher)(f)
+	}
+
+	if f.kicks <= 0 {
+		Kicks(defaultKicks)(f)
+	}
+}
--- cuckoo.go
+++ cuckoo.go
@@ -1,1 +1,22 @@
 package cuckoo
+
+import (
+	"hash"
+)
+
+type Filter struct {
+	bucketEntries     uint
+	bucketTotal       uint
+	fingerprintLength uint
+	hasher            hash.Hash
+	kicks             uint
+}
+
+func New(opts ...configOption) (filter *Filter) {
+	filter = &Filter{}
+	for _, option := range opts {
+		option(filter)
+	}
+	filter.configureDefaults()
+	return
+}
