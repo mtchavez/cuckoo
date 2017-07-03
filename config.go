15d0fe1b5774e3c1a34abea75ee0b2a84314702d
Chavez <matthew@el-chavez.me>
2017-07-03 10:37:01 -0700

    Filter insert
    - Add bucket type of fingerprints
    - Add fingerprint type of byte slice
    - Configure filter with capacity calculated
    - Add farmhash for hasing of fingerprints
    - Filter insert with relocations

--- config.go
+++ config.go
@@ -7,11 +7,23 @@ import (
 
 type configOption func(*Filter)
 
+// Cuckoo Filter notation
+// e target false positive rate
+// f fingerprint length in bits
+// α load factor (0 ≤ α ≤ 1)
+// b number of entries per bucket
+// m number of buckets
+// n number of items
+// C average bits per item
 const (
-	defaultBucketEntries     uint = 8
-	defaultBucketTotal       uint = 2 ^ 30
+	// Entries per bucket (b)
+	defaultBucketEntries uint = 24
+	// Bucket total (m) defaults to approx. 4 million
+	defaultBucketTotal uint = 1 << 22
+	// Length of fingreprint (f) set to log(n/b) ~6 bits
 	defaultFingerprintLength uint = 6
-	defaultKicks             uint = 500
+	// Default attempts to find empty slot on insert
+	defaultKicks uint = 500
 )
 
 var (
@@ -48,13 +60,22 @@ func Kicks(kicks uint) configOption {
 	}
 }
 
-func (f *Filter) configureDefaults() {
-	if f.bucketEntries <= 0 {
-		BucketEntries(defaultBucketEntries)(f)
+func capacity() configOption {
+	return func(f *Filter) {
+		f.capacity = nextPowerOf2(uint64(f.bucketTotal)) / f.bucketEntries
+		if f.capacity <= 0 {
+			f.capacity = 1
+		}
 	}
+}
 
+func (f *Filter) configureDefaults() {
 	if f.bucketTotal <= 0 {
 		BucketTotal(defaultBucketTotal)(f)
+	}
+
+	if f.bucketEntries <= 0 {
+		BucketEntries(defaultBucketEntries)(f)
 	}
 
 	if f.fingerprintLength <= 0 {
@@ -68,4 +89,20 @@ func (f *Filter) configureDefaults() {
 	if f.kicks <= 0 {
 		Kicks(defaultKicks)(f)
 	}
+
+	if f.capacity < 1 {
+		capacity()(f)
+	}
+}
+
+func nextPowerOf2(n uint64) uint {
+	n--
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	n |= n >> 32
+	n++
+	return uint(n)
 }
